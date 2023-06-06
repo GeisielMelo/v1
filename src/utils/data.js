acd8f7fecdb1fc19bf66697d0249bc1dabac04c4
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-06 15:24:33 -0300

    New file to store all data about projects and repositories.

---
+++ src/utils/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    title: "Project 1",
+    description: "Project 1 description goes here",
+    technologies: ["React", "Node.js", "MongoDB", "Express"],
+    gitLink: "https://github.com/your-username/project1",
+    imageLink: "https://example.com/project1-image.png",
+    liveLink: "https://example.com/project1-live-demo",
+    featured: true,
+  },
+  {
+    title: "Project 2",
+    description: "Project 2 description goes here",
+    technologies: ["Python", "Django", "PostgreSQL", "AWS"],
+    gitLink: "https://github.com/your-username/project2",
+    imageLink: "https://example.com/project2-image.png",
+    liveLink: null,
+    featured: true,
+  },
+  {
+    title: "Project 3",
+    description: "Project 2 description goes here",
+    technologies: ["Python", "Django", "PostgreSQL", "AWS"],
+    gitLink: "https://github.com/your-username/project2",
+    imageLink: "https://example.com/project2-image.png",
+    liveLink: "https://example.com/project2-live-demo",
+    featured: true,
+  },
+];
+
+export default data;
