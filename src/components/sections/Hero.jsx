2f023df13cc36a705e9ff788de4e5572a70600e1
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-07 15:21:02 -0300

    Tag that holds the "job title" has been changed h2 to h3.

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = (props) => {
       <div>
         <h1>{props.title}</h1>
         <h2>{props.name}</h2>
-        <h2>{props.job}</h2>
+        <h3>{props.job}</h3>
         <p>{props.intro}</p>
       </div>
     </section>
