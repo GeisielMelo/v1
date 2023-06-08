919f789ca36670b36a1f942524a25f5fe1f570b4
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-08 00:32:10 -0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,24 +1,23 @@
 const config = {
-  navBarLogoLink: "https://img.icons8.com/ios/50/circled-g.png",
-  navBar: ["About", "Tech", "Projects", "Contact",],
+  navBar: ["About", "Featured", "Projects", "Contact",],
   navBarButton: "Resume",
   
   appTitle: "Hi, my name is",
   name: "Geisiel Melo",
-  job: "Software Engineer",
-  introduction: "I’m a software engineer specializing in building.",
+  job: "I build web applications.",
+  introduction: "I'm a software engineer with experience in web application development. I'm currently seeking new opportunities to apply my skills and knowledge. With a special focus on creating accessible and user-centric solutions, I am committed to building products that stand out and provide amazing experiences for users.",
 
   aboutTitle: "About Me!",
-  bio: "LoremLoremLoremLoremLoremLoremLorem",
-  experience: "LoremLoremLoremLoremLoremLoremLoremLoremLorem",
-  conclusion: "LoremLoremLorem",
+  bio: "Hello, I'm Geisiel Melo, and I love coding and problem-solving. My programming journey began with configuring custom mods and servers for online games, bringing enjoyment to my friends and me. Over time, I ventured into creating scripts to automate tasks and eventually developed full-fledged applications for complex challenges.",
+  experience: "As I delved into app development, I discovered my passion for creating solutions that benefit others. This led me to focus on web application development, aiming to provide accessible and user-friendly experiences to a broader audience.",
+  conclusion: "Sharing my applications and witnessing how they simplify people's lives motivates me greatly. I firmly believe in the positive impact of technology on society, and I'm dedicated to crafting intuitive, efficient, and enjoyable web applications.",
 
   techTitle: "Here are a few technologies I’ve been working with recently:",
   tech: ["JavaScript", "NodeJS", "Python",  "React",  "Express",  "MongoDB",  "MySQL"],
   imageLink: "https://img.freepik.com/vector-premium/icono-doodle-logo-dibujo-perfil-masculino-aislado-sobre-fondo-oscuro_159242-392.jpg",
   imageLinkDescription: "a male person",
 
-  featuredTitle: "Some projects that i build.",
+  featuredTitle: "Featured Projects",
 
   projectsTitle: "Other Noteworthy Projects",
   projectsSubTitle: "view the archive",
