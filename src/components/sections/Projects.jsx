0e2fb11d958560eff9889c4e892fae61a01fdedd
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-08 00:34:34 -0300

    Added setting on the see more button to load new project cards.

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card";
 
 const Projects = (props) => {
+  const data  = props.data;
+  const [displayedItems, setDisplayedItems] = useState(6);
+
+  const handleMoreButtonClick = () => {
+    setDisplayedItems(displayedItems + 3);
+  };
+
   return (
     <section id="projects">
       <div className="title-projects">
@@ -9,20 +16,22 @@ const Projects = (props) => {
         <p>{props.subTitle}</p>
       </div>
       <div className="cards-area">
-        {props.data.map((item) => (
+        {data.slice(0, displayedItems).map((item) => (
           <Card
             key={item.title}
             title={item.title}
             description={item.description}
             technologies={item.technologies}
-            live={item.liveLink}
             github={item.gitLink}
+            live={item.liveLink}
           />
         ))}
       </div>
-      <div className="more-button">
-        <button>{props.button}</button>
-      </div>
+      {displayedItems < data.length && (
+        <div className="more-button">
+          <button onClick={handleMoreButtonClick}>{props.button}</button>
+        </div>
+      )}
     </section>
   );
 };
