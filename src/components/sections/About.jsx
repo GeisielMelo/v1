9389539ef6c6c51aaeb726bc4ed8c9a0c27263c1
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-08 00:41:12 -0300

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,22 +4,21 @@ import Tech from "../Tech";
 const About = (props) => {
   return (
     <section id="about">
-      <div>
+      <div className="horizontal-line-title">
         <h1>{props.title}</h1>
+        <div></div>
       </div>
+
       <div className="about-container">
         <div className="content">
           <p>{props.bio}</p>
-          <p>{props.work}</p>
-          <p>{props.experience}.</p>
+          <p>{props.experience}</p>
+          <p>{props.conclusion}.</p>
           <p>{props.techTitle}</p>
           <Tech technologies={props.tech} />
         </div>
         <div className="grid-photo">
-          <img
-            src={props.image}
-            alt={props.imageAlt}
-          />
+          <img src={props.image} alt={props.imageAlt} />
         </div>
       </div>
     </section>
