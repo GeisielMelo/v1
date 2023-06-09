02c43bcf86b8b29e98665a518cc55ed98ed3cc80
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-09 00:23:13 -0300

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,12 +4,14 @@ import Tech from "../Tech";
 const About = (props) => {
   return (
     <section id="about">
+
       <div className="horizontal-line-title">
         <h1>{props.title}</h1>
         <div></div>
       </div>
 
       <div className="about-container">
+
         <div className="content">
           <p>{props.bio}</p>
           <p>{props.experience}</p>
@@ -17,10 +19,12 @@ const About = (props) => {
           <p>{props.techTitle}</p>
           <Tech technologies={props.tech} />
         </div>
+
         <div className="grid-photo">
           <img src={props.image} alt={props.imageAlt} />
         </div>
       </div>
+      
     </section>
   );
 };
