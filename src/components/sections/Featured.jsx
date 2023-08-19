95edc538e7c6c5c4bb09285b7a3d6324f96bc47e
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-08-19 18:45:45 -0300

    feat: Rebuilt the Featured component for simplicity.

--- src/components/sections/Featured.jsx
+++ src/components/sections/Featured.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Title } from "../Title";
 
 const Section = styled.section`
@@ -12,38 +14,48 @@ const Section = styled.section`
   margin: 0 100px;
   padding: 100px 0px;
   max-width: 850px;
-  width: 100%;
-  height: 100vh;
+  width: calc(100% - 200px);
 
-  @media (max-width: 1068px) {
-    padding: 100px 50px;
+  @media (max-width: 769px) {
+    margin: 0 50px;
+    width: calc(100% - 100px);
   }
 `;
 
 const Container = styled.div`
   display: flex;
   margin: 50px 0;
-
-  @media (max-width: 1068px) {
+  @media (max-width: 1060px) {
     flex-direction: column-reverse;
   }
 `;
 
 const Menu = styled.div`
-  max-width: 200px;
-  max-height: 400px;
+  max-width: 150px;
   width: 100%;
-  height: 100%;
-  @media (max-width: 1068px) {
-    max-width: 400px;
+  @media (max-width: 1060px) {
+    max-width: 100%;
+  }
+`;
+
+const Preview = styled.div`
+  position: relative;
+  max-width: 700px;
+  max-height: 400px;
+  @media (max-width: 1060px) {
+    border-radius: 8px;
+    max-width: 100%;
+    max-height: 100%;
   }
 `;
 
 const List = styled.ul`
   list-style: none;
   cursor: pointer;
-  @media (max-width: 1068px) {
+  @media (max-width: 1060px) {
     display: flex;
+    flex-wrap: wrap;
+    gap: 5px;
   }
 `;
 
@@ -51,8 +63,8 @@ const Item = styled.li`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
   height: 40px;
+  padding: 10px;
   font-family: ${(props) => props.theme.font.family.one};
   font-size: ${(props) => props.theme.font.size.sm};
   border-left: 2px solid ${(props) => props.theme.color.Slate.default};
@@ -62,26 +74,19 @@ const Item = styled.li`
     border-color: ${(props) => props.theme.color.Green.default};
     background-color: ${(props) => props.theme.color.Navy.Light};
   }
-  @media (max-width: 1068px) {
+  @media (max-width: 1060px) {
     border-left: none;
-    padding: 5px;
-    margin: 5px;
     border-bottom: 2px solid ${(props) => props.theme.color.Slate.default};
   }
-`;
-
-const Preview = styled.div`
-  position: relative;
-  max-width: 650px;
-  max-height: 400px;
-  width: 100%;
-  height: 100%;
 `;
 
 const Image = styled.img`
   width: 100%;
   height: 100%;
   box-shadow: 0 1px 5px rgba(2, 12, 27, 0.7);
+  @media (max-width: 1060px) {
+    border-radius: 8px;
+  }
 `;
 
 const Description = styled.div`
@@ -94,7 +99,10 @@ const Description = styled.div`
   border-radius: 8px;
   color: white;
   padding: 10px;
-  margin: 5px;
+  margin: 9px 5px;
+  &:hover {
+    background-color: ${(props) => props.theme.color.Navy.Transparent};
+  }
 `;
 
 const DescriptionText = styled.p`
@@ -141,9 +149,24 @@ const Buttons = styled.div`
   }
 `;
 
+const Visibility = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  position: absolute;
+  text-align: right;
+  top: 0px;
+  right: 0px;
+  margin: 9px 5px;
+  color: ${(props) => props.theme.color.Slate.Lightest};
+`;
 
 const Featured = ({ settings, data }) => {
   const [previewData, setPreviewData] = useState(null);
+  const [showDescription, setShowDescription] = useState(true);
   const isFeatured = (item) => item.Featured === true;
 
   useEffect(() => {
@@ -160,6 +183,7 @@ const Featured = ({ settings, data }) => {
   return (
     <Section>
       <Title title={settings.Title} />
+
       <Container>
         <Menu>
           <List>
@@ -170,9 +194,11 @@ const Featured = ({ settings, data }) => {
             ))}
           </List>
         </Menu>
+
         <Preview>
           <Image src={previewData?.ImageLink} />
-
+          
+          {showDescription && (
           <Description>
             <DescriptionText>{previewData?.Description}</DescriptionText>
             <DescriptionTech>
@@ -193,6 +219,13 @@ const Featured = ({ settings, data }) => {
               )}
             </Buttons>
           </Description>
+          )}
+          <Visibility
+            onMouseEnter={() => setShowDescription(false)}
+            onMouseLeave={() => setShowDescription(true)}
+          >
+            {showDescription ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </Visibility>
         </Preview>
       </Container>
     </Section>
