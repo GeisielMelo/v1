7913cb6035353eb6a35c03605431aa23e9c85afa
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-08-18 23:57:39 -0300

    feat: Completed the refactor of the Projects component.

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,10 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Card } from "../Card";
 
-const Projects = () => {
-    return (
-        <div>Projects</div>
-    )
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 100px 0px;
+  margin: 0 100px;
+  max-width: 1000px;
+  width: 100%;
+
+  @media (max-width: 1068px) {
+    padding: 100px 50px;
+    max-width: 760px;
+  }
+`;
+
+const Title = styled.h1`
+  max-width: max-content;
+  white-space: nowrap;
+  font-family: ${(props) => props.theme.font.family.two};
+  color: ${(props) => props.theme.color.Slate.Lightest};
+`;
+
+const Subtitle = styled.h2`
+  margin: 10px;
+  max-width: max-content;
+  margin-bottom: 30px;
+  padding-left: 4px;
+
+  font-family: ${(props) => props.theme.font.family.one};
+  font-size: ${(props) => props.theme.font.size.sm};
+  font-weight: ${(props) => props.theme.font.weight.regular};
+  color: ${(props) => props.theme.color.Green.default};
+`;
+
+const Cards = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+  @media (max-width: 1068px) {
+    justify-content: center;
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 50px;
+  padding: 20px 30px;
+  background: none;
+  border-radius: 5px;
+  cursor: pointer;
+  color: ${(props) => props.theme.color.Green.default};
+  border: 1px solid ${(props) => props.theme.color.Green.default};
+
+  &:hover {
+    background: ${(props) => props.theme.color.Navy.Lightest};
+  }
+`;
+
+const Projects = ({ settings, data }) => {
+  const [maxCards, setMaxCards] = useState(6);
+
+  return (
+    <Section>
+      <Title>{settings.Title}</Title>
+      <Subtitle>{settings.Subtitle}</Subtitle>
+      <Cards>
+        {data?.slice(0, maxCards).map((item, index) => (
+          <Card
+            key={index}
+            title={item.Title}
+            description={item.Description}
+            technologies={item.Technologies}
+            git={item.GitLink}
+            live={item.LiveLink}
+          />
+        ))}
+      </Cards>
+      {maxCards < data.length ? (
+        <Button onClick={() => setMaxCards(maxCards + 3)}>
+          {settings.ButtonMore}
+        </Button>
+      ) : (
+        <Button onClick={() => setMaxCards(6)}>{settings.ButtonLess}</Button>
+      )}
+    </Section>
+  );
 };
 
 export default Projects;
